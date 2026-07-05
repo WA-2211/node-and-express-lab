2fd4f64fb0bf6db9aa5452eee9e304e0e2644482
WA-2211 <walaaalghanmi@gmail.com>
2026-07-05 15:44:42 +0300

    Lab Completed

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 const express = require('express')
 const app = express()
+
+app.use(express.static('public'));
+
+app.get('/test',(req,res) =>{
+    res.send('<p>Success!</p>')
+})
+
+// app.get('/', (req,res)=>{
+//     res.send('Welcome to my application Walaa')
+// })
+
+// app.get('/about', (req, res)=>{
+//     res.send('Walaa Idress, a cybersecurity student ')
+// })
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/about', (req, res)=>{
+    res.sendFile(__dirname + '/views/about.html')
+})
+
+app.listen(3000, console.log('App is running'))
